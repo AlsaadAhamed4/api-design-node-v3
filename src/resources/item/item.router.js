eddbd7fee32303fbcfca8dc2dee034d4fee11e91
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-09-12 17:18:44 +0530

    Added a Item router with mock controller
    - registered route in main server app

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Router } from 'express'
 
+const controller = (req, res) => {
+    res.send({ message: "hello" });
+}
+
 const router = Router()
+// /api/item
+
+router
+    .route('/')
+    .get(controller)
+    .post(controller)
+
+router
+    .route('/:id')
+    .put(controller)
+    .delete(controller)
+    .get(controller)
+
 
 export default router
--- src/server.js
+++ src/server.js
@@ -4,11 +4,8 @@ import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
-import { signup, signin, protect } from './utils/auth'
 import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
-import listRouter from './resources/list/list.router'
 
 export const app = express()
 
@@ -19,13 +16,8 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.post('/signup', signup)
-app.post('/signin', signin)
-
-app.use('/api', protect)
-app.use('/api/user', userRouter)
+// loaded here and registered to main route
 app.use('/api/item', itemRouter)
-app.use('/api/list', listRouter)
 
 export const start = async () => {
     try {
