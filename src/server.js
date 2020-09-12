ef4e54c67aec6bb7b5f971ae0ae60bf62dce4bac
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-09-12 16:21:25 +0530

    Learnt about Express.js, routers, middleware.
    - used Express verbs to create server with routes .
    - user router from express and sub routing using middlewares with use()
    - Rest routes of 5 , 2 in express js
    - registering a middleware with use() and calling a req/res ontrollers

--- src/server.js
+++ src/server.js
@@ -2,38 +2,92 @@
 import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
-import config from './config'
 import cors from 'cors'
-import { signup, signin, protect } from './utils/auth'
-import { connect } from './utils/db'
-import userRouter from './resources/user/user.router'
-import itemRouter from './resources/item/item.router'
-import listRouter from './resources/list/list.router'
 
-export const app = express()
+export const app = express();
+
+const router = express.Router(); // this how we initaialize router in express.
 
 app.disable('x-powered-by')
+
+
 
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-app.post('/signup', signup)
-app.post('/signin', signin)
+// this act as an middleware 
+// we can execute this middle before the controller 
+// eg: app.get('/data',log,(req,res)=>{})
+// we can pass array of middlewares [log,test]....
+const log = (req, res, next) => {
+    console.log(req.body, 'logging');
+    next(); // this tell to continue executing next thing
+}
 
-app.use('/api', protect)
-app.use('/api/user', userRouter)
-app.use('/api/item', itemRouter)
-app.use('/api/list', listRouter)
+// app.use(log); // this saying execute this middleware before every controller
 
-export const start = async () => {
-    try {
-        await connect()
-        app.listen(config.port, () => {
-            console.log(`REST API on http://localhost:${config.port}/api`)
-        })
-    } catch (e) {
-        console.error(e)
-    }
+// routing in express with different routes like regx, exact match, glob, paramtrs
+
+app.get('/users/*', (req, res) => {
+    res.send({ message: 'route for glob' });
+})
+
+app.get('/:id', (req, res) => {
+    console.log(req.params);
+    res.send({ message: 'route for params' });
+})
+
+app.get('/data', log, (req, res) => {
+    res.send({
+        message: "Hello"
+    })
+})
+
+app.post('/data', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+// example for routers in express.
+// if we are using router we need to register it with root royter which is app
+
+// basically we are saying anything with /api execute router
+app.use('/api', router);
+
+// to go to this we use /api/me 
+router.get('/me', (req, res) => {
+    res.send({ me: 'hello' });
+})
+// end of router example
+
+// we can also have middleware for router 
+// router.use('/route',anyReqResFunc);
+
+
+
+// rest routes there are 5 
+
+const routes = ['get /cat', 'get /cat:id', 'post /cat', 'put /cat:id', 'delete cat/:id'];
+
+// express does this in 2 ways , we can use router or app
+
+// 1
+router.route('/cat')
+    .get()
+    .post()
+
+// 2 
+router.route('/cat/:id')
+    .get()
+    .put()
+    .delete()
+
+
+// server 
+export const start = () => {
+    app.listen(3000, () => {
+        console.log('Server is running');
+    })
 }
