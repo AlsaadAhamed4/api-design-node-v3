48cebd697fb48ed2b74732cd2186ad8b56ff7242
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-09-13 13:38:59 +0530

    Created routes with crud controller & crud operation for each routes.
    - Added schema for item model
    - wrote CRUD for requests.

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,5 @@
-export default {}
+/* eslint-disable prettier/prettier */
+import { crudControllers } from '../../utils/crud'
+import { Item } from './item.model'
+
+export default crudControllers(Item)
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,67 @@
-export const getOne = model => async (req, res) => {}
+/* eslint-disable prettier/prettier */
 
-export const getMany = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user._id;  // we need user as well only specified user can have their data.
 
-export const createOne = model => async (req, res) => {}
+  // in item model we have createdBy where we specify user ref
+  const docs = await model.findOne({ _id: id, createdBy: userId }).exec();
 
-export const updateOne = model => async (req, res) => {}
+  if (!docs) {
+    return res.status(404).end(); // the response should be ended
+  }
+  res.status(200).json({ data: docs });
+}
 
-export const removeOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const docs = await model.find({ createdBy: req.user._id }).exec();
+  if (!docs) {
+    return res.status(404).end();
+  }
+  res.status(200).json({ data: docs });
+}
+
+export const createOne = model => async (req, res) => {
+  const doc = model.create({ ...req.body, createdBy: req.user._id });
+  res.status(200).json({ data: doc });
+}
+
+export const updateOne = model => async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user._id;
+
+  const updatedDoc = await model.findByIdAndUpdate(
+    // fields
+    {
+      _id: id,
+      createdBy: userId
+    },
+    // updates
+    req.body,
+    // return back
+    { new: true }
+  ).exec();
+
+  if (!updatedDoc) {
+    return res.status(404).end();
+  }
+  res.status(200).json({ data: updatedDoc });
+}
+
+export const removeOne = model => async (req, res) => {
+
+  const id = req.params.id;
+  const userId = req.user._id;
+
+  const doc = await model.findOneAndRemove({ _id: id, createdBy: userId }).exec();
+
+  if (!doc) {
+    return res.status(404).end();
+  }
+
+  res.status(200).json({ data: doc });
+
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
